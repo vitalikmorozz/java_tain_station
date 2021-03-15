a7f52ec82ccf3a7be300ebc4fdcdf2d4a3997fa0
Vitalii Moroz <vitalii.m@code-care.pro>
2021-03-15 19:31:52 +0200

    Model mapper configurations

--- src/main/java/moroz/project/train/config/ModelMapperConfiguration.java
+++ src/main/java/moroz/project/train/config/ModelMapperConfiguration.java
@@ -1,6 +1,8 @@
 package moroz.project.train.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,9 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfiguration {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+        return modelMapper;
     }
 }
